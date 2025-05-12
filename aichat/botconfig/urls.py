40b5f77143c633d89347d79fbea7f4085aced166
paavkar <paavo_karppinen@outlook.com>
2025-05-12 22:52:29 +0300

    update template page, add CSS

--- aichat/botconfig/urls.py
+++ aichat/botconfig/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update_config/', views.update_config_view, name='update_config'),
-    path('get_config/', views.get_config_view, name='get_config'),
+    path('update-config/', views.update_config_view, name='update_config'),
+    path('get-config/', views.get_config_view, name='get_config'),
     path('config/', views.display_config_view, name='display-config')
 ]
